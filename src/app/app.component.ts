5e2641c0339a60a4dc8c7f50c62233a6d160d9b9
Perry Anderson <perry.anderson@gmail.com>
2017-05-30 15:13:29 -0300

    How to properly reset an Angular 2 form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,12 +48,16 @@ export class AppComponent {
   // }
 
   onSubmit() {
+
     this.submitted = true;
     this.user.username = this.signupForm.value.userData.username;
     this.user.email = this.signupForm.value.userData.email;
     this.user.secretQuestion = this.signupForm.value.secret;
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
+
   }
 
 }
